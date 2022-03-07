67640fd71a671b55af74dfef1b6d4b535e4c7c27
Tharindu <ku.tharindu@gmail.com>
2022-03-07 18:04:35 +0530

    added redux

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 export default function Home(){
+    useEffect(() =>{
+        console.log(">>>>>>>>>>")
+        setFf(console.log("setState"))
+    },[]);
+    console.log("RRRRRRRRRRRRR1")
+const[ff, setFf] = useState(console.log("useStatw"))
+console.log("RRRR22222222")
+
+
+    console.log("RRRRRRRRRRRRR333333333333333")
+
     return(
-        <p>Home</p>
+        <p>Homes</p>
     )
 }
